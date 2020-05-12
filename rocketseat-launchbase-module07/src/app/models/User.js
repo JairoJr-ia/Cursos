1bdfaa357958cd69d18d0e7384a92d8e5877069d
Jairo Antonio dos Santos Junior <jairo_juniorjj@hotmail.com>
2020-05-12 09:06:01 -0300

    Finalizado Cadastro de usuario

--- rocketseat-launchbase-module07/src/app/models/User.js
+++ rocketseat-launchbase-module07/src/app/models/User.js
@@ -1,4 +1,5 @@
 const db = require('../../config/db')
+const { hash } = require('bcryptjs')
 
 module.exports = {
     async findOne(filters) {
@@ -22,6 +23,42 @@ module.exports = {
       return results.rows[0]
 
     },
+    async create(data){
+      try{
+        const query = `
+        INSERT INTO users (   
+            name,
+            email,
+            password,
+            cpf_cnpj,
+            cep,
+            address
 
+        ) VALUES ($1, $2, $3, $4, $5, $6)   
+            RETURNING id
+        `
+
+        // hash of password
+        const passwordHash = await hash(data.password, 8)
+
+        const values = [
+            data.name,
+            data.email || 1,
+            passwordHash,
+            data.cpf_cnpj.replace(/\D/g, ''),
+            data.cep.replace(/\D/g, ''),
+            data.address,    
+
+        ]
+
+        const results = await db.query(query, values)
+
+        return results.rows[0].id
+
+      } catch (e) {
+        console.error((e));
+        
+      }
+    },
 
 };
